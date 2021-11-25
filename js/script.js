48d836749f8e71e02b787f651bcbf045834415d0
Alex <adrdra88@gmail.com>
2021-11-25 16:09:46 +0100

    playing with functions

--- js/script.js
+++ js/script.js
@@ -2,7 +2,17 @@ const app = new Vue({
     el:'#root',
     data:{
         message:'hello Vue!!!',
+        myClass:'blue',
         source:'https://images.unsplash.com/photo-1516259762381-22954d7d3ad2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1189&q=80'
-    }
-})
+    },
+    methods:{
+        myTogle: function() {
+        if(this.myClass == 'blue'){
+            this.myClass = 'red';
+        }else if(this.myClass == 'red'){
+            this.myClass = 'green';
+        }else{this.myClass = 'blue'}
+    
+    }}
+});
 
